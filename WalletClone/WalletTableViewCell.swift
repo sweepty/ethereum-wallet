d8c64a5849c5241ca4123fe3a3ce2a36c936b02a
sweepty <adie0423@gmail.com>
2019-05-15 18:01:25 +0900

    Add address copy button

--- WalletClone/WalletTableViewCell.swift
+++ WalletClone/WalletTableViewCell.swift
@@ -7,16 +7,25 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class WalletTableViewCell: UITableViewCell {
 
     @IBOutlet weak var walletNameLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var balanceLabel: UILabel!
+    @IBOutlet weak var copyAddressButton: UIButton!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        copyAddressButton.rx.tap.asControlEvent()
+            .subscribe { (_) in
+                UIPasteboard.general.string = self.addressLabel.text
+        }
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
